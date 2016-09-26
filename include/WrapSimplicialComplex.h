e0d01475ace84a9c5f64f5a122586f60f61a5515
ctlee <ctlee@ucsd.edu>
2016-09-26 11:03:11 -0700

    Changing the wrap to c++ for better object representation in python

--- include/WrapSimplicialComplex.h
+++ include/WrapSimplicialComplex.h
@@ -1,23 +1,16 @@
 
 #pragma once
 
-typedef void CASC;
-
-#ifdef __cplusplus
-extern "C"{
-#endif
-
-CASC* newSimplicialComplex();
-void deleteSimplicialComplex(CASC* casc);
-void insertVertex(CASC* casc, unsigned int n, double x, double y, double z);
-void insertFace(CASC* casc, unsigned int a, unsigned int b, unsigned int c);
-int numVertices(CASC* casc);
-int numFaces(CASC* casc);
-
-/*
-void insertFaceRGB(CASC* casc, unsigned int a, unsigned int b, unsigned int c, 
-        double color_r, double color_g, double color_b);
-*/
-#ifdef __cplusplus
-}
-#endif
+class  WrappedSimplicialComplex{
+public:
+    WrappedSimplicialComplex();
+    ~WrappedSimplicialComplex();
+    template<typename KeyType> 
+    void insertVertex(KeyType n, double x, double y, double z);
+    template<typename KeyType>
+    void insertFace(KeyType a, KeyType b, KeyType c);
+    int numVertices();
+    int numFaces();
+private:
+    void* ptr_asc;
+};
--- src/WrapSimplicialComplex.C
+++ src/WrapSimplicialComplex.C
@@ -2,59 +2,58 @@
 #include "Util.h"
 #include "WrapSimplicialComplex.h"
 
+struct Vec3 {
+    double x;
+    double y;
+    double z;
+};
 
-extern "C" {
-    struct Vec3 {
-        double x;
-        double y;
-        double z;
-    };
-    
-    struct Orientable {
-        int orientation;
-    };
+struct Orientable {
+    int orientation;
+};
 
-    struct Face : Orientable {
-        Face(int orient, long m) : Orientable{orient}, mark(m) {}
-        long mark;
-    };
+struct Face : Orientable {
+    Face(int orient, long m) : Orientable{orient}, mark(m) {}
+    long mark;
+};
 
-    struct complex_traits
-    {
-        using KeyType = size_t;
-        using NodeTypes = util::type_holder<int,Vec3,int, Orientable>;
-        using EdgeTypes = util::type_holder<Orientable,Orientable,Orientable>;
-    };
-    using ASC = simplicial_complex<complex_traits>; 
+struct complex_traits
+{
+    using KeyType = size_t;
+    using NodeTypes = util::type_holder<int,Vec3,int, Orientable>;
+    using EdgeTypes = util::type_holder<Orientable,Orientable,Orientable>;
+};
+using ASC = simplicial_complex<complex_traits>; 
+using WSC = WrappedSimplicialComplex;
 
-    CASC* newSimplicialComplex(){
-        ASC* asc = new ASC();
-        return (CASC*)asc;
+WSC::WrappedSimplicialComplex(){
+    ASC* asc =  new ASC();
+    this->ptr_asc = (void*) asc;
+    }
+WSC::~WrappedSimplicialComplex(){
+    ASC* asc = (ASC*) this->ptr_asc; 
+    delete asc;
     }
 
-    void deleteSimplicialComplex(CASC* casc){
-        ASC* asc = (ASC*) casc; 
-        delete asc;
-    }
+template<> void WSC::insertVertex<>(complex_traits::KeyType n, 
+        double x, double y, double z){
+    ASC* asc = (ASC*) this->ptr_asc; 
+    Vec3 v{x,y,z};
+    asc->insert<1>({n},v);
+}
 
-    void insertVertex(CASC* casc, unsigned int n, double x, double y, double z){
-        ASC* asc = (ASC*) casc;
-        Vec3 v{x,y,z};
-        asc->insert<1>({n},v);
-    }
+template<> void WSC::insertFace<>(complex_traits::KeyType a, 
+        complex_traits::KeyType b, complex_traits::KeyType c){
+    ASC* asc = (ASC*) this->ptr_asc; 
+    asc->insert<3>({a,b,c});
+}
 
-    void insertFace(CASC* casc, unsigned int a, unsigned int b, unsigned int c){
-        ASC* asc = (ASC*) casc;
-        asc->insert<3>({a,b,c});
-    }
+int WSC::numVertices(){
+    ASC* asc = (ASC*) this->ptr_asc; 
+    return asc->size<1>(); 
+}
 
-    int numVertices(CASC* casc){
-        ASC* asc = (ASC*) casc;
-        return asc->size<1>(); 
-    }
-
-    int numFaces(CASC* casc){
-        ASC* asc = (ASC*) casc;
-        return asc->size<3>();
-    }
+int WSC::numFaces(){
+    ASC* asc = (ASC*) this->ptr_asc; 
+    return asc->size<3>();
 }
