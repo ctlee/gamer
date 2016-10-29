e3d472f8e079fb7546346c6b88897b361eedc511
John B Moody <brogan@gmail.com>
2016-10-28 19:09:59 -0700

    Possible implementation of getTangent

--- tests/scratchtest_jbm.cpp
+++ tests/scratchtest_jbm.cpp
@@ -8,6 +8,35 @@
 #include <iostream>
 #include <string>
 
+
+
+template <std::size_t dimension>
+auto getTangentH(SurfaceMesh& mesh, const tensor<double, dimension, 1>& origin, SurfaceMesh::NodeID<SurfaceMesh::topLevel> curr)
+{
+    return 1.0;
+}
+
+template <std::size_t level, std::size_t dimension>
+auto getTangentH(SurfaceMesh& mesh, const tensor<double, dimension, 1>& origin, SurfaceMesh::NodeID<level> curr)
+{
+    tensor<double, dimension, SurfaceMesh::topLevel - level> rval;
+
+    for(auto alpha : mesh.get_cover(curr))
+    {
+        auto edge = *mesh.get_edge_up(curr, alpha);
+        const auto& v = (*mesh.get_node_up({alpha})).position;
+        auto next = mesh.get_node_up(curr,alpha);
+        rval += edge.orientation * (v-origin) * getTangentH(mesh, origin, next);
+    }
+
+    return rval;
+}
+
+auto getTangent_jbm(SurfaceMesh& mesh, SurfaceMesh::NodeID<1> vertexID)
+{
+    return getTangentH(mesh, (*vertexID).position, vertexID);
+}
+
 int main(int argc, char *argv[])
 {
     if(argc != 2)
@@ -23,9 +52,16 @@ int main(int argc, char *argv[])
         exit(1);
     }
     auto mesh = result.first;
+
+    init_orientation(*mesh);
+    clear_orientation(*mesh);
+    compute_orientation(*mesh);
     
     for(auto v : mesh->get_level_id<1>())
     {
+        auto T = getTangent_jbm(*mesh, v);
+        std::cout << T << std::endl;
+        /*
         auto edges = mesh->up(v);
         auto faces = mesh->up(edges);
 
@@ -34,5 +70,6 @@ int main(int argc, char *argv[])
         {
             std::cout << "    " << f << std::endl;
         }
+        */
     }
 }
