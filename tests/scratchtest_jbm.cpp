86865cfc36dc3439e76d02f6e3ba96e88675a3fd
ctlee <ctlee@ucsd.edu>
2016-10-28 20:53:11 -0700

    Merging in the correct getTangent

--- tests/scratchtest_jbm.cpp
+++ tests/scratchtest_jbm.cpp
@@ -9,34 +9,6 @@
 #include <string>
 
 
-
-template <std::size_t dimension>
-auto getTangentH(SurfaceMesh& mesh, const tensor<double, dimension, 1>& origin, SurfaceMesh::NodeID<SurfaceMesh::topLevel> curr)
-{
-    return 1.0;
-}
-
-template <std::size_t level, std::size_t dimension>
-auto getTangentH(SurfaceMesh& mesh, const tensor<double, dimension, 1>& origin, SurfaceMesh::NodeID<level> curr)
-{
-    tensor<double, dimension, SurfaceMesh::topLevel - level> rval;
-
-    for(auto alpha : mesh.get_cover(curr))
-    {
-        auto edge = *mesh.get_edge_up(curr, alpha);
-        const auto& v = (*mesh.get_node_up({alpha})).position;
-        auto next = mesh.get_node_up(curr,alpha);
-        rval += edge.orientation * (v-origin) * getTangentH(mesh, origin, next);
-    }
-
-    return rval;
-}
-
-auto getTangent_jbm(SurfaceMesh& mesh, SurfaceMesh::NodeID<1> vertexID)
-{
-    return getTangentH(mesh, (*vertexID).position, vertexID);
-}
-
 int main(int argc, char *argv[])
 {
     if(argc != 2)
@@ -59,7 +31,7 @@ int main(int argc, char *argv[])
     
     for(auto v : mesh->get_level_id<1>())
     {
-        auto T = getTangent_jbm(*mesh, v);
+        auto T = getTangent(*mesh, v);
         std::cout << T << std::endl;
         /*
         auto edges = mesh->up(v);
