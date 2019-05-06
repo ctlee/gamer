e83ea9992557bf99c7f219935b7006dcf66d1c91
Christopher Lee <ctlee@ucsd.edu>
2019-05-05 21:26:44 -0700

    Missed one

--- cmake-modules/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justin/gitrepos/gamer
-# Build directory: /home/justin/gitrepos/gamer/cmake-modules
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libraries")
-subdirs("swig")
-subdirs("tests")
